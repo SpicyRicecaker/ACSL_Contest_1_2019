ef3e73bdfc73cc40c9aec2f1dd5b0276514bfa96
SpicyRicecaker <awesomenessisgolden@gmail.com>
2019-12-21 09:59:12 -0800

    Did the problem but the amount of code used was excessive

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        File input = new File("input.txt");
+        try {
+            String inLine;
+            BufferedReader br = new BufferedReader(new FileReader(input));
+            while ((inLine = br.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(inLine);
+                while (st.hasMoreTokens()) {
+                    String tempP = st.nextToken();
+                    ArrayList<Integer> P = new ArrayList<>();
+                    for (int a = 0; a < tempP.length(); ++a) {
+                        P.add(tempP.charAt(a) - '0');
+                    }
+                    int N = P.size() - (Integer.parseInt(st.nextToken()));
+                    ArrayList<Integer> result = new ArrayList<>();
+                    for (int a = 0; a < P.size(); ++a) {
+                        if (a < N) {
+                            result.add(P.get(a) % 10);
+                        } else if (a == N) {
+                            result.add(P.get(a));
+                        } else {
+                            result.add(Math.abs(P.get(a) - P.get(N)));
+                        }
+                    }
+                    for (Integer integer : result) System.out.print(integer);
+                    System.out.println();
+                }
+            }
+            br.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found.");
+            System.exit(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
